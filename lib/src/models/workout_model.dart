f926394840023ed155f3ef7d6a36396fc49abbc9
Axel COUDRAY <40031504+acoudray1@users.noreply.github.com>
2019-02-19 15:33:04 +0100

    Update workout_model.dart

--- lib/src/models/workout_model.dart
+++ lib/src/models/workout_model.dart
@@ -1,43 +1,10 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
-import 'package:muscle/general/enum/enum_feedback.dart';
-import 'package:muscle/general/enum/enum_type_of_workout.dart';
-import 'package:muscle/workout/controlleur/class_routine.dart';
+import 'package:figura/src/models/enum/enum_feedback.dart';
+import 'package:figura/src/models/enum/enum_type_of_workout.dart';
+import 'package:figura/src/models/routine_model.dart';
 
 // TODO: Ajouter la possibilité d'écrire son propre type de workout dans son fichier json (par user) -> newTypeOfWorkout(String str)
-
-/// Method used to get a json file and write it inside a String, then write this string inside a list of workout and returns it
-///
-/// @param : (String) Path of the json file (The json file must be added to the pubspec.yaml)
-/// @return Future<List<Workout>>
-///
-Future<List<Workout>> fetchWorkouts(String path) async {
-  final String response = await rootBundle.loadString(path);
-
-  // Use the compute function to run parsePhotos in a separate isolate
-  return workoutFromJson(response);
-}
-
-/// Method used to parse a json file string and return a list of workout
-/// 
-/// @param : String of a json file
-/// @return : List<Workout>
-///
-List<Workout> workoutFromJson(String str) {
-    final dynamic jsonData = json.decode(str);
-    return List<Workout>.from(jsonData.map((dynamic x) => Workout.fromJson(x)));
-}
-
-/// Method used to write a workout as a json 
-///
-/// @param : List<Workout>
-/// @return : json
-///
-String workoutToJson(List<Workout> data) {
-    final List<dynamic> dyn = List<dynamic>.from(data.map<dynamic>((dynamic x) => x.toJson()));
-    return json.encode(dyn);
-}
-
 class Workout {
 
   Workout({this.routines, this.name, this.description, this.typeOfWorkout});
