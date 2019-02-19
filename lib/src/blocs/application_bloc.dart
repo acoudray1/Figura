63580ca78891a7a5eef1a9c38f1aff5b50737e3c
Axel COUDRAY <40031504+acoudray1@users.noreply.github.com>
2019-02-19 15:53:45 +0100

    Update application_bloc.dart

--- lib/src/blocs/application_bloc.dart
+++ lib/src/blocs/application_bloc.dart
@@ -1,6 +1,31 @@
 import 'package:figura/src/blocs/bloc_provider.dart';
+import 'package:rxdart/rxdart.dart';
 
 class ApplicationBloc extends BlocBase{
+  
+  // Appel du repository depuis lequel on peut appeler les méthodes utilisées pour fetch des json
+  final Repository _repository = Repository();
+  
+  // On crée nos deux 'StreamControler' qui sont en fait des PublishSubject -> return un 'Observable' plutot qu'un stream et 
+  // peut être écouté plusieurs fois. Equivalent à un StreamController en broadcast.
+  final PublishSubject<List<Exercise>> _exercisesFetcher = PublishSubject<List<Exercise>>();
+  final PublishSubject<List<Workout>> _workoutsFetcher = PublishSubject<List<Workout>>();
+  
+  // Listener des PublishSubject créés précédemment
+  final Observable<List<Exercise>> get allExercises => _exercisesFetcher.stream;
+  final Observable<List<Workout>> get allWorkouts => _workoutsFetcher.stream;
+  
+  // Methodes appelant les différentes méthodes 'fetch' de repository afin d'envoyer dans le PublishSubject le résultat
+  fetchAllExercises() async  {
+    List<Exercise>> exercises = await _repository.fetchAllExercises();
+    _exercisesFetcher.add(exercises);
+  }
+  
+  fetchAllWorkouts() async  {
+    List<Workout>> workouts = await _repository.fetchAllWorkouts();
+    _workoutssFetcher.add(workouts);
+  }
+  
   
   @override
   void dispose() {
