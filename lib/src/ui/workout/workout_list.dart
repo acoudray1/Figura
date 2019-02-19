10d9ba94ade6c8bb9e31ff87606a1521b87f2e30
Axel COUDRAY <40031504+acoudray1@users.noreply.github.com>
2019-02-19 16:48:28 +0100

    Create workout_list.dart

---
+++ lib/src/ui/workout/workout_list.dart
@@ -0,0 +1,30 @@
+
+
+class WorkoutList extends StatefulWidget {
+
+  @override
+  _WorkoutListState createState() => _WorkoutListState();
+}
+
+class _WorkoutListState extends State<WorkoutList> {
+  // Creation du bloc controller 
+  final WorkoutBloc workoutBloc; 
+  // Création de ma liste de workout à afficher
+  List<Workout> workouts = <Workout>[];
+  
+  @override
+  void initState() {
+    super.initState();
+    // initialisation du bloc 
+    workoutBloc = WorkoutBloc(); 
+    // Appel de la méthode à l'initialisation de la page
+    workoutBloc.fetchAllWorkouts();
+  }
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+
+}
