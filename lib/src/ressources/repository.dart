9e434ca74d132656d8b7f2db92eb7f956c648ce3
Axel COUDRAY <40031504+acoudray1@users.noreply.github.com>
2019-02-19 15:37:19 +0100

    Update repository.dart

--- lib/src/ressources/repository.dart
+++ lib/src/ressources/repository.dart
@@ -1,12 +1,16 @@
 
 import 'package:figura/src/ressources/exercise_provider.dart';
 import 'package:figura/src/models/exercise_model.dart';
+import 'package:figura/src/ressources/workout_provider.dart';
+import 'package:figura/src/models/workout_model.dart';
 
 /// This repository class is the central point from where the data will flow to BLOC
 /// 
 class Repository {
 
   final ExerciseProvider exerciseProvider = ExerciseProvider();
+  final WorkoutProvider workoutProvider = WorkoutProvider();
 
   Future<List<Exercise>> fetchAllExercises() => exerciseProvider.fetchExercises('assets/exercises.json');
+  Future<List<Workout>> fetchAllWorkouts() => workoutProvider.fetchWorkouts('...');
 }
