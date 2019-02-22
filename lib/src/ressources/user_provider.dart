3d3a235c193863a0ea9fa651f0a069e69481715e
Axel COUDRAY <40031504+acoudray1@users.noreply.github.com>
2019-02-22 16:28:29 +0100

    Update user_provider.dart

--- lib/src/ressources/user_provider.dart
+++ lib/src/ressources/user_provider.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 
 class UserProvider {
-	/// Method used to get a json file and write it inside a String, then write this string inside a list of exercises and returns it
+  /// Method used to get a json file and write it inside a String, then write this string inside a list of exercises and returns it
   ///
   /// @param : (String) Path of the json file (The json file must be added to the pubspec.yaml)
   /// @return Future<User>
@@ -12,25 +12,25 @@ class UserProvider {
 
     // Use the compute function to run parsePhotos in a separate isolate
     return userFromJson(response);
-	}
+  }
 	
-	/// Method used to parse a json file string and return a list of exercises
+  /// Method used to parse a json file string and return a list of exercises
   /// 
   /// @param : String of a json file
   /// @return : User
-	///
-	User userFromJson(String str) {
-			final jsonData = json.decode(str);
-			return User.fromJson(jsonData);
-	}
+  ///
+  User userFromJson(String str) {
+    final jsonData = json.decode(str);
+    return User.fromJson(jsonData);
+  }
 
-	/// Method used to write a json by using a list of exercises
+  /// Method used to write a json by using a list of exercises
   ///
   /// @param : List<User>
   /// @return : String of a json file
-	///
-	String userToJson(User data) {
-			final dyn = data.toJson();
-			return json.encode(dyn);
-	}
+  ///
+  String userToJson(User data) {
+    final dyn = data.toJson();
+    return json.encode(dyn);
+  }
 }
