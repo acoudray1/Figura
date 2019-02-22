0e251b4cf606868f1fe8d05abd577710e0bc2f2e
Axel COUDRAY <40031504+acoudray1@users.noreply.github.com>
2019-02-22 16:25:59 +0100

    Update user_model.dart

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -12,6 +12,7 @@ class User {
     User({
         this.userFirstname,
         this.userLastname,
+        this.sex,
         this.age,
         this.workoutPerWeekGoal,
         this.measures,
@@ -23,27 +24,30 @@ class User {
     factory User.fromJson(Map<String, dynamic> json) => new User(
         userFirstname: json["user_firstname"],
         userLastname: json["user_lastname"],
+        sex: sexValues.map[json['SexValues']],
         age: json["age"],
         workoutPerWeekGoal: json["workout_per_week_goal"],
-        measures: new List<Map<String, double>>.from(json["measures"].map((x) => new Map.from(x).map((k, v) => new MapEntry<String, double>(k, v.toDouble())))),
-        measuresGoal: new List<Map<String, double>>.from(json["measures_goal"].map((x) => new Map.from(x).map((k, v) => new MapEntry<String, double>(k, v.toDouble())))),
+        measures: List<Map<String, double>>.from(json["measures"].map((x) => Map.from(x).map((k, v) => MapEntry<String, double>(k, v.toDouble())))),
+        measuresGoal: List<Map<String, double>>.from(json["measures_goal"].map((x) => Map.from(x).map((k, v) => MapEntry<String, double>(k, v.toDouble())))),
     );
     
     String userFirstname;
     String userLastname;
+    Sex sex;
     int age;
     int workoutPerWeekGoal;
-    List<Map<String, double>> measures;
-    List<Map<String, double>> measuresGoal;
+    List<Measures> measures;
+    List<MeasuresGoal> measuresGoal;
     
     /// Write in a Json files
     /// 
     Map<String, dynamic> toJson() => {
         "user_firstname": userFirstname,
         "user_lastname": userLastname,
+        "Sex": sexValues.reverse[sex],
         "age": age,
         "workout_per_week_goal": workoutPerWeekGoal,
-        "measures": new List<dynamic>.from(measures.map((x) => new Map.from(x).map((k, v) => new MapEntry<String, dynamic>(k, v)))),
-        "measures_goal": new List<dynamic>.from(measuresGoal.map((x) => new Map.from(x).map((k, v) => new MapEntry<String, dynamic>(k, v)))),
+        "measures": List<dynamic>.from(measures.map((x) => Map.from(x).map((k, v) => MapEntry<String, dynamic>(k, v)))),
+        "measures_goal": List<dynamic>.from(measuresGoal.map((x) => Map.from(x).map((k, v) => MapEntry<String, dynamic>(k, v)))),
     };
 }
