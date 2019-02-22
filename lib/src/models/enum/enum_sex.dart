d40168d67a7eb9c60f708ede3e117476a7fcf159
Axel COUDRAY <40031504+acoudray1@users.noreply.github.com>
2019-02-22 15:18:56 +0100

    Update enum_sex.dart

--- lib/src/models/enum/enum_sex.dart
+++ lib/src/models/enum/enum_sex.dart
@@ -1,3 +1,9 @@
 enum Sex {
   FEMALE, MALE, OTHER,
 }
+
+final EnumValues<Sex> sexValues = EnumValues<Sex> (<String, Sex> {
+    'FEMALE': Sex.FEMALE,
+    'MALE': Sex.MALE,
+    'OTHER': Sex.OTHER,
+});
